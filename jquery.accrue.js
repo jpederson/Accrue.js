346e6d803566f45bb6ae6f15cedc3c8fec232095
sepiariver <info@sepiariver.com>
2016-01-14 14:28:41 -0800

    function to calculate loan amount

--- jquery.accrue.js
+++ jquery.accrue.js
@@ -447,5 +447,54 @@
             return 0;
         }
     };
+    
+    
+    
+    // REVERSE LOAN INFORMATION FUNCTION
+    // This is a copy of the above, only that given a payment amount, rate and term it
+    // will return the principal amount that can be borrowed.
+    $.loanAmount = function( input ) {
+
+        var payment = ( typeof( input.payment )!=="undefined" ? input.payment : 0 ).toString().replace(/[^\d.]/ig, ''),
+            rate = ( typeof( input.rate )!=="undefined" ? input.rate : 0 ).toString().replace(/[^\d.]/ig, ''),
+            term = ( typeof( input.term )!=="undefined" ? input.term : 0 );
+
+        // parse year values passed into the term value
+        if ( term.match("y") ) {
+            term = parseInt( term.replace(/[^\d.]/ig, ''), 10 )*12;
+        } else {
+            term = parseInt( term.replace(/[^\d.]/ig, ''), 10 );
+        }
+
+        // process the input values
+        var monthly_interest = rate / 100 / 12,
+            annual_interest = rate / 100;
+
+        // Now compute.
+        var x = payment * (1 - Math.pow(1 + monthly_interest, -1 * term)) * (12/(annual_interest));
+                
+        // If the result is a finite number, the user's input was good and
+        // we have meaningful results to display
+        if ( x>0 ) {
+            // Fill in the output fields, rounding to 2 decimal places
+            return {
+                principal_amount: x,
+                principal_amount_formatted: ( x * 1 ).toFixed(2),
+                payment_amount: payment,
+                payment_amount_formatted: ( payment * 1 ).toFixed(2),
+                num_payments: term,
+                total_payments: ( payment * term ), 
+                total_payments_formatted: ( payment * term ).toFixed(2), 
+                total_interest: ( ( payment * term ) - x ),
+                total_interest_formatted: ( ( payment * term ) - x ).toFixed(2)
+            };
+
+        } else {
+            // The numbers provided won't provide good data as results,
+            // so we'll return 0 so it's easy to test if one of the fields
+            // is empty or invalid.
+            return 0;
+        }
+    };
 
 })( jQuery, window, document );
